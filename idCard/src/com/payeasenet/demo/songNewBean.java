c40def1fbdfd6def5fe15e37539c870f66b4c22a
songweijie-win <songweijie-win@windows10.microdone.cn>
2017-07-19 16:11:48 +0800

    greate a bean

---
+++ idCard/src/com/payeasenet/demo/songNewBean.java
@@ -0,0 +1,29 @@
+/**
+ * songNewBean.java 2017年7月19日
+ * 
+ * 渤海易购（天津）科技发展有限公司(c) 2016 - 2017 。
+ * http://www.bhaigou.com
+ *
+ */
+package com.payeasenet.demo;
+
+
+/**
+ * <b>songNewBean。</b>
+ * <p><b>详细说明：</b></p>
+ * <!-- 在此添加详细说明 -->
+ * 无。
+ * <p><b>修改列表：</b></p>
+ * <table width="100%" cellSpacing=1 cellPadding=3 border=1>
+ * <tr bgcolor="#CCCCFF"><td>序号</td><td>作者</td><td>修改日期</td><td>修改内容</td></tr>
+ * <!-- 在此添加修改列表，参考第一行内容 -->
+ * <tr><td>1</td><td>万程</td><td>2017年7月19日 下午4:07:15</td><td>建立类型</td></tr>
+ * 
+ * </table>
+ * @version 1.0
+ * @author 宋纬杰
+ * @since 1.0
+ */
+public class songNewBean {
+
+}
